4a0f28921615d713d976af1d2078efca5c51a4bd
Mircea Lungu <mircea.lungu@gmail.com>
2021-03-11 14:57:04 +0100

    log to stdout by default

--- apimux/constants.py
+++ apimux/constants.py
@@ -1,4 +1,4 @@
-LOGGER_NAME = 'apimux'
+LOGGER_NAME = "apimux"
 
 # Log level of apimux:
 #   1 - CRITICAL
@@ -8,7 +8,8 @@ LOGGER_NAME = 'apimux'
 #   5 - DEBUG
 LOG_LEVEL = 5
 
-LOG_TO_FILE = "apimux.log"
+# LOG_TO_FILE = "apimux.log"
+LOG_TO_FILE = None
 
 # Periodic check of the response time of the API
 PERIODIC_CHECK = 3  # seconds
